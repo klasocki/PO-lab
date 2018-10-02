90eb605452825c06ffc994f417ca7b877c07e3b1
klasocki <karolasocki@gmail.com>
2018-10-02 13:34:08 +0200

    Add run method, informing about car movement

--- src/agh/cs/lab1/CarSystem.java
+++ src/agh/cs/lab1/CarSystem.java
@@ -4,8 +4,12 @@ public class CarSystem {
     public static void main(String[] args) {
         System.out.println("System start");
 
-
+        run();
 
         System.out.println("System finish");
+    }
+
+    private static void run() {
+        System.out.println("Car is moving forward");
     }
 }
