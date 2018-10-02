6b5faac833327904def1eae3e07a6c0eb2247da5
klasocki <karolasocki@gmail.com>
2018-10-02 13:32:17 +0200

    Add program start and stop messages

--- src/agh/cs/lab1/CarSystem.java
+++ src/agh/cs/lab1/CarSystem.java
@@ -1,4 +1,11 @@
 package agh.cs.lab1;
 
 public class CarSystem {
+    public static void main(String[] args) {
+        System.out.println("System start");
+
+
+
+        System.out.println("System finish");
+    }
 }
