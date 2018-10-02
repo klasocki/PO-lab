fd242a94a7dd8aeb3f667aa585bfe85339c091ad
klasocki <karolasocki@gmail.com>
2018-10-02 14:19:29 +0200

    Change strings to enums in run method

--- src/agh/cs/lab1/CarSystem.java
+++ src/agh/cs/lab1/CarSystem.java
@@ -4,30 +4,62 @@ public class CarSystem {
     public static void main(String[] args) {
         System.out.println("System start");
 
-        String[] runArguments = {"Very", "Very", "Fast"};
 
+        Direction[] runArguments = new Direction[args.length];
+        for (int i=0; i<args.length; i++) {
+            try {
+                runArguments[i] = stringToDir(args[i]);
+            } catch (Exception e) {
+                System.out.println("Wrong argument type: " + args[i]);
+
+            }
+        }
         run(runArguments);
-        run(args);
+
 
         System.out.println("System finish");
     }
 
-    private static void run(String[] args) {
-        for (String dir : args) {
-            switch (dir) {
-                case "f":
-                    System.out.println("Moving forward");
-                    break;
-                case "b":
-                    System.out.println("Moving backward");
-                    break;
-                case "l":
-                    System.out.println("Moving left");
-                    break;
-                case "r":
-                    System.out.println("Moving right");
-                    break;
-            }
+    private static void run(Direction[] args) {
+        for (Direction dir : args) {
+
+            try {
+                switch (dir) {
+                    case FORWARD:
+                        System.out.println("Moving forward");
+                        break;
+                    case BACKWARD:
+                        System.out.println("Moving backward");
+                        break;
+                    case LEFT:
+                        System.out.println("Moving left");
+                        break;
+                    case RIGHT:
+                        System.out.println("Moving right");
+                        break;
+                }
+            } catch (Exception e) { }
         }
+    }
+
+    private static Direction stringToDir(String dir) {
+        Direction result;
+        switch (dir) {
+            case "f":
+                result= Direction.FORWARD;
+            break;
+            case "b":
+                result= Direction.BACKWARD;
+            break;
+            case "l":
+                result= Direction.LEFT;
+            break;
+            case "r":
+                result= Direction.BACKWARD;
+            break;
+            default:
+                throw new RuntimeException("Wrong direction keyword");
+        }
+       return result;
     }
 }
