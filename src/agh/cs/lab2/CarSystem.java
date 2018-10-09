38826f582a4b5214a617f6b965e436ab3a508c1f
klasocki <karolasocki@gmail.com>
2018-10-09 13:28:49 +0200

    Add required methods to Position class* *According to the instruction

--- src/agh/cs/lab2/CarSystem.java
+++ src/agh/cs/lab2/CarSystem.java
@@ -2,6 +2,6 @@ package agh.cs.lab2;
 
 public class CarSystem {
     public static void main(String[] args) {
-
+        System.out.println(new Position(4,-5).toString());
     }
 }
--- src/agh/cs/lab2/Position.java
+++ src/agh/cs/lab2/Position.java
@@ -1,5 +1,7 @@
 package agh.cs.lab2;
 
+import javafx.geometry.Pos;
+
 public class Position {
     public final int x;
     public final int y;
@@ -7,6 +9,36 @@ public class Position {
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
-        
+    }
+
+    @Override
+    public String toString() {
+        return "(" + this.x + "," + this.y + ")";
+    }
+
+    public boolean smaller(Position other) { return other.x<this.x && other.y<this.y; }
+    public boolean larger(Position other) { return other.x>this.x && other.y>this.y; }
+
+    public Position upperRight(Position other) {
+        int newX = Math.max(this.x, other.x);
+        int newY = Math.max(this.y, other.y);
+        return new Position(newX, newY);
+    }
+
+    public Position lowerLeft(Position other) {
+        int newX = Math.min(this.x, other.x);
+        int newY = Math.min(this.y, other.y);
+        return new Position(newX, newY);
+    }
+
+    public Position add(Position other) {
+        return new Position(this.x + other.x, this.y + other.y);
+    }
+
+    public boolean equals(Object other) {
+        if(this==other) return true;
+        if(!(other instanceof Position)) return false;
+        Position another = (Position) other;
+        return another.x == this.x && another.y == this.y;
     }
 }
