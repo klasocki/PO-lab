0a33f2638f1ea1492f30f2b7536a1c942a7490b9
klasocki <karolasocki@gmail.com>
2018-10-30 13:16:48 +0100

    Add HayStack class, representing an obstacle type on the map

---
+++ src/main/lab5/HayStack.java
@@ -0,0 +1,20 @@
+package lab5;
+
+import agh.cs.lab2.Position;
+
+public class HayStack {
+    private Position position;
+
+    public HayStack(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "s";
+    }
+}
